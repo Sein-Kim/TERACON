c837631f607a298691b552565beac17a8d3632e9
Sein-Kim <rlatpdlsgns81@gmail.com>
2023-07-19 18:42:52 +0900

    readme

--- ttl_inference.sh
+++ ttl_inference.sh
@@ -2,3 +2,4 @@ python inference_past_tasks.py --datapath "./ColdRec/original_desen_pretrain.csv
 python inference_past_tasks.py --datapath "./ColdRec/original_desen_finetune_click_nouserID.csv" --datapath_index "./Data/Session/index.csv" --paths "./saved_models/task5.t7" --n_tasks 5 --inference_task 2
 python inference_past_tasks.py --datapath "./ColdRec/original_desen_finetune_like_nouserID.csv" --datapath_index "./Data/Session/index.csv" --paths "./saved_models/task5.t7" --n_tasks 5 --inference_task 3
 python inference_past_tasks.py --datapath "./ColdRec/original_desen_age.csv" --datapath_index "./Data/Session/index.csv" --paths "./saved_models/task5.t7" --n_tasks 5 --inference_task 4
+python inference_past_tasks.py --datapath "./ColdRec/original_desen_lifestatus.csv" --datapath_index "./Data/Session/index.csv" --paths "./saved_models/task5.t7" --n_tasks 5 --inference_task 5
