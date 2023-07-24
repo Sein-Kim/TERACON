8da3cf713f2bc3734d84f760bdbc574ed4516e4b
Sein-Kim <rlatpdlsgns81@gmail.com>
2023-07-24 21:49:00 +0900

    modi

--- ttl_train.sh
+++ ttl_train.sh
@@ -1,4 +1,4 @@
-python python train_task1.py --epochs 10 --lr 0.001 --batch 32
+python train_task1.py --epochs 10 --lr 0.001
 python train_teracon.py --lr 0.001 --smax 50 --batch 1024 --datapath "./ColdRec/original_desen_finetune_click_nosuerID.csv" --datapath_index "./Data/Session/index.csv" --paths "./saved_models/task1.t7" --savepath "./saved_models/task2" --n_tasks 2
 python train_teracon.py --lr 0.0001 --smax 50 --batch 1024 --datapath "./ColdRec/original_desen_finetune_like_nosuerID.csv" --datapath_index "./Data/Session/index.csv" --paths "./saved_models/task2.t7" --savepath "./saved_models/task3" --n_tasks 3
 python train_teracon.py --lr 0.0001 --smax 50 --batch 1024 --datapath "./ColdRec/original_desen_age.csv" --datapath_index "./Data/Session/index.csv" --paths "./saved_models/task3.t7" --savepath "./saved_models/task4" --n_tasks 4
