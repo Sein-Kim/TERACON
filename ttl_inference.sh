b48e074e9f004717a3458e2c3da12960134ee158
Sein-Kim <rlatpdlsgns81@gmail.com>
2023-10-16 15:02:53 +0900

    Inferecne shell

--- ttl_inference.sh
+++ ttl_inference.sh
@@ -3,3 +3,11 @@ python inference_past_tasks.py --datapath "./ColdRec/original_desen_finetune_cli
 python inference_past_tasks.py --datapath "./ColdRec/original_desen_finetune_like_nouserID.csv" --datapath_index "./Data/Session/index.csv" --paths "./saved_models/task5.t7" --n_tasks 5 --inference_task 3
 python inference_past_tasks.py --datapath "./ColdRec/original_desen_age.csv" --datapath_index "./Data/Session/index.csv" --paths "./saved_models/task5.t7" --n_tasks 5 --inference_task 4
 python inference_past_tasks.py --datapath "./ColdRec/original_desen_lifestatus.csv" --datapath_index "./Data/Session/index.csv" --paths "./saved_models/task5.t7" --n_tasks 5 --inference_task 5
+
+#For all tasks
+# python inference_past_tasks.py --datapath "./ColdRec/original_desen_pretrain.csv" --datapath_index "./Data/Session/index.csv" --paths "./saved_models/task6.t7" --n_tasks 6 --inference_task 1
+# python inference_past_tasks.py --datapath "./ColdRec/original_desen_finetune_click_nouserID.csv" --datapath_index "./Data/Session/index.csv" --paths "./saved_models/task6.t7" --n_tasks 6 --inference_task 2
+# python inference_past_tasks.py --datapath "./ColdRec/original_desen_finetune_like_nouserID.csv" --datapath_index "./Data/Session/index.csv" --paths "./saved_models/task6.t7" --n_tasks 6 --inference_task 3
+# python inference_past_tasks.py --datapath "./ColdRec/original_desen_age.csv" --datapath_index "./Data/Session/index.csv" --paths "./saved_models/task6.t7" --n_tasks 6 --inference_task 4
+# python inference_past_tasks.py --datapath "./ColdRec/original_desen_lifestatus.csv" --datapath_index "./Data/Session/index.csv" --paths "./saved_models/task6.t7" --n_tasks 6 --inference_task 5
+# python inference_past_tasks.py --datapath "./ColdRec/original_desen_gender.csv" --datapath_index "./Data/Session/index.csv" --paths "./saved_models/task6.t7" --n_tasks 6 --inference_task 6
